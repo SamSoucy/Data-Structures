76b8147e44917e98e4f8ba30a734c366c9e0d135
Samsoucy <newf930@outlook.com>
2019-05-16 18:42:36 -0400

    finished heap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -3,19 +3,51 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
+    self._bubble_up(self.get_size() -1)
+   
 
   def delete(self):
-    pass
+    self.storage[0], self.storage[len(self.storage) - 1] = self.storage[len(self.storage) - 1], self.storage[0]
+    value_to_return = self.storage.pop()
+    self._sift_down(0)
+    return value_to_return
 
   def get_max(self):
-    pass
+    return self.storage[0]
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   def _bubble_up(self, index):
-    pass
+    while index > 0:
+      parent = (index - 1) // 2
+      if self.storage[index] > self.storage[parent]:
+            self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+            index = parent
+      else:
+        break
 
   def _sift_down(self, index):
-    pass
+    if (self.get_size() - 1) >= ((2 * index) + 1):
+          if self.storage[(2 * index) + 1] > self.storage[index]:
+                if (self.get_size() - 1) >= ((2 * index) + 2):
+                      if self.storage[(2 * index) + 2] > self.storage[(2 * index) + 1]:
+                            self.storage[(2 * index) + 2], self.storage[index] = self.storage[index], self.storage[(2 * index) + 2]
+                            self._sift_down((2 * index) + 2)
+                      else:
+                            self.storage[(2 * index) + 1], self.storage[index] = self.storage[index], self.storage[(2 * index) + 1]
+                            self._sift_down((2 * index) + 1)
+                else:
+                  self.storage[(2 * index) + 1], self.storage[index] = self.storage[index], self.storage[(2 * index) + 1]
+                  self._sift_down((2 * index) + 1)
+          else:
+            if (self.get_size() - 1) >= ((2 * index) + 2):
+              if self.storage[(2 * index) + 2] > self.storage[index]:
+                self.storage[(2 * index) + 2], self.storage[index] = self.storage[index], self.storage[(2 * index) + 2]
+                self._sift_down((2 * index) + 2)
+              else:
+                return
+            else:
+              return
+          
